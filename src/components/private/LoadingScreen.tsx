bbd35cb4e0dc6fe542f50e1d30b7633b4eaa5635
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-12-13 08:03:29 +0100

    mace

--- src/components/private/LoadingScreen.tsx
+++ src/components/private/LoadingScreen.tsx
@@ -2,22 +2,21 @@ import { BallTriangle } from "react-loader-spinner";
 
 const LoadingScreen = () => {
   return (
-    <div className="">
-      <div
-        className="w-full h-screen flex items-center justify-center flex-col "
-        style={{ backdropFilter: "blur(4px)" }}
-      >
-        <BallTriangle
-          height={100}
-          width={100}
-          radius={5}
-          color="#4fa94d"
-          ariaLabel="ball-triangle-loading"
-          visible={true}
-        />
-        <div className="mt-10 font-[Zah]">Getting datas....</div>
-      </div>
+    <div
+      className="w-full h-screen flex items-center absolute z-[500] justify-center flex-col "
+      style={{ backdropFilter: "blur(4px)" }}
+    >
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#4fa94d"
+        ariaLabel="ball-triangle-loading"
+        visible={true}
+      />
+      <div className="mt-10 font-[Zah] text-white">Getting datas....</div>
     </div>
+    // </div>
   );
 };
 
